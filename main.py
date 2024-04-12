0760951ec53559b62851d797a36b659881cdb8f4
Grisell Pinkston <pinkstonsophia@ufl.edu>
2024-04-12 14:33:20 -0400

    added in decode method, and modified code to integrate it

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ def encode(password):
             encoded_password_string += digit
         return encoded_password_string
 
-
+# Sophia Pinkston
+def decode(encoded_password):
+	encoded_password_list = []
+	original_password_str = ""
+	encoded_password_list[:] = encoded_password
+	for element in encoded_password_list:
+		element = int(element)
+		element -= 3
+		element = str(element)
+		original_password_str += element
+	return original_password_str
+# fixed spelling error in encode, and added in the lines that save the passwords in order to use the f string's 
 def main():
     while True:
         password = ""
@@ -20,13 +31,13 @@ def main():
         print()
         user_input = int(input("Please enter an option: "))
         if user_input == 1:
-            password = input("Please enter your password to enocde: ")
-            encode(password)
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
             print("Your password has been encoded and stored!")
             print()
         elif user_input == 2:
-            #decode()
-            print(f"The encoded password is , and the original password is {password}")
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}")
         elif user_input == 3:
             break
 
