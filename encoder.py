d2df44cbc5e2b488fe73f6822aa65fac00c8d95b
galindosarah <166773304+galindosarah@users.noreply.github.com>
2024-04-12 13:00:47 -0400

    Add files via upload

---
+++ encoder.py
@@ -0,0 +1,14 @@
+# Sarah Galindo
+
+
+def encode(password):
+    if len(password) == 8:
+        og_password_digit_list = []
+        encoded_password_string = ""
+        og_password_digit_list[:] = password
+        for digit in og_password_digit_list:
+            digit = int(digit)
+            digit += 3
+            digit = str(digit)
+            encoded_password_string += digit
+        return encoded_password_string
