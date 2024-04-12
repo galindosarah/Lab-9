b02eea6c775c484c2167d5f03a391aff8b789f91
Sarah Galindo <CHESS101@MacBook-Pro-3.local>
2024-04-12 13:18:12 -0400

    combined encoder into main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # Sarah Galindo
-
-
-from encoder import encode
+def encode(password):
+    if len(password) == 8:
+        og_password_digit_list = []
+        encoded_password_string = ""
+        og_password_digit_list[:] = password
+        for digit in og_password_digit_list:
+            digit = int(digit)
+            digit += 3
+            digit = str(digit)
+            encoded_password_string += digit
+        return encoded_password_string
 
 
 def main():
